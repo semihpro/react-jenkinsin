f8397f06a3f4034e20b8e6520f841ae6d76672f4
SEMIH <semihcetin34@gmail.com>
2020-09-21 14:18:32 +0300

    did you understand commit

--- src/App.js
+++ src/App.js
@@ -8,8 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hello deleted
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. Did you understand?
         </p>
         <a
           className="App-link"
