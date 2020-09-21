2f2953e0b0f551b4b2c40c6f69c4f5c8f3a9e62d
SEMIH <semihcetin34@gmail.com>
2020-09-21 15:30:42 +0300

    sfsdf

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>yy
+        <p>yytttt
           Edit <code>src/App.js</code> and save to reload. 5. commit
         </p>
         <a
