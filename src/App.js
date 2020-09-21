ee27c6f10832ee1e2af5832144efacdf7d448245
SEMIH <semihcetin34@gmail.com>
2020-09-21 16:11:37 +0300

    bakalım update edecek mi

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>yytttttthhh
-          Edit <code>src/App.js</code> and save to reload. 5. commit
+        <p>
+          Edit and save to reload. 5. commit
         </p>
         <a
           className="App-link"
