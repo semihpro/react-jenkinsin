0c62cbd99e9a553cc6efa6f7d97ac44beb53f5f3
SEMIH <semihcetin34@gmail.com>
2020-09-21 17:01:45 +0300

    ttttt

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit and save to reload. 5. commit
+          Edit and save to reload. 5. committttt
         </p>
         <a
           className="App-link"
